d4dde2854fa2abe5f9fb690d6634d10dfae43f75
Doxirth <0mar.uthman0304@gmail.com>
2025-11-30 09:01:18 +0700

    TechArt v.6.0
    changed the way status effects notify player.

--- Assets/Asset FIX UI UI UI/Script UI dimari/UI InGame/EntityStatEffectHolder.cs
+++ Assets/Asset FIX UI UI UI/Script UI dimari/UI InGame/EntityStatEffectHolder.cs
@@ -1,8 +1,13 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class EntityStatEffectHolder : MonoBehaviour
 {
+    RectTransform rectTrans;
+    Vector3 baseScale;
+    Vector3 currentScale;
+    [SerializeField] AnimationCurve resizeAnimCurve;
     [SerializeField] Image imgComp;
     public Image GetImageComponent() => imgComp;
     [SerializeField] Moves currentMove;
@@ -12,6 +17,8 @@ public class EntityStatEffectHolder : MonoBehaviour
     {
         currentMove = new Moves();
         imgComp = GetComponent<Image>();
+        rectTrans = transform?.GetComponent<RectTransform>();
+        currentScale = rectTrans.localScale;
     }
 
     public void UpdateStatEffect(Moves type, Sprite statEffectIMG, bool isXpired)
@@ -30,8 +37,26 @@ public class EntityStatEffectHolder : MonoBehaviour
             imgComp.sprite = statEffectIMG;
             imgComp.color = new Color(imgComp.color.r, imgComp.color.g, imgComp.color.b, 1f);
             currentMove.affectedStat = type.affectedStat;
+            if (CO_ResizeUI != null) return;
+            CO_ResizeUI = StartCoroutine(StartResizeUI());
         }
 
-        //Debug.LogError($"stat effect has expired? {isXpired}!");
+    }
+
+    Coroutine CO_ResizeUI;
+    IEnumerator StartResizeUI()
+    {
+        float timer = 0f;
+        float maxTimer = resizeAnimCurve[resizeAnimCurve.length - 1].time;
+        while (timer <= maxTimer)
+        {
+            timer += Time.deltaTime;
+            var flt_T = timer/maxTimer;
+            var flt_CurveEval = resizeAnimCurve.Evaluate(flt_T);
+            rectTrans.localScale = currentScale * flt_CurveEval;
+            yield return null;
+        }
+        currentScale = rectTrans.localScale;
+        CO_ResizeUI = null;
     }
 }
